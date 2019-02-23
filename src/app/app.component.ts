73b49815250adabe600acdf02c94a4d24e776240
ruta.sakalauskaite@qustodio.com <ruta.sakalauskaite@qustodio.com>
2019-02-23 18:48:43 +0100

    app implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng6-proj';
+
+export class AppComponent implements OnInit {
+  suggestions: any[];
+  suggestion: string;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.suggestion = '';
+    this.suggestions = [];
+  }
+
+  likeSuggestion(sugg) {
+    sugg.likes += 1;
+    sugg.liked = true;
+  }
+
+  addSuggestion() {
+    this.suggestions.push({'suggestion': this.suggestion, 'likes': 0, 'liked': false});
+  }
 }
